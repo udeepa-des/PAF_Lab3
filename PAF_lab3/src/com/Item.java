975ac3ae62f3744ca11f6f619ed5a28bbdc63e0f
udeepa-des <ududeepa@gmail.com>
2021-03-22 15:58:27 +0530

    commit 2

--- PAF_lab3/src/com/Item.java
+++ PAF_lab3/src/com/Item.java
@@ -1,25 +1,21 @@
 package com;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.*;
 
 public class Item {
 	
-	public Connection connect() {
-		Connection con = null;
-		
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/test","root", "");
-			
-			System.out.println("Connected Successfully");
-		}catch(Exception e) {
+	public Connection connect(){ 
+		Connection con = null; 
+		try        
+		{ Class.forName("com.mysql.jdbc.Driver");
+		con= DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/test", "root", "");    
+		//For testing
+		System.out.print("Successfully connected");
+		} catch(Exception e)          { 
 			e.printStackTrace();
-		}
-		
-		return con;
-				
+			} 
+		return con; 
+			
 	}
 
 }
